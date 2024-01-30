d4dafc8648bc9219614a7616a2124b014727f8e7
Nikolas-Vardeberg <post@nikolas.no>
2024-01-30 21:05:34 +0100

    tailwind-merge, clsx config

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+    return(
+        <h1>admin</h1>
+    )
+}
+
+export default page
---
+++ src/app/booking/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+    return(
+        <h1>booking</h1>
+    )
+}
+
+export default page
